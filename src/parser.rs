494bef9d581b0f3ab365c6e31d59330f662ef0e2
kgv <kgv@users.noreply.github.com>
2025-03-03 17:15:10 +0300

    Bump version to 0.2.0

---
+++ src/parser.rs
@@ -0,0 +1,31 @@
+use std::{collections::VecDeque, iter::from_fn};
+
+const RADIX: u32 = 10;
+
+pub(crate) fn tokenize(input: &str) -> VecDeque<Token> {
+    let mut tokens = VecDeque::new();
+    let mut iter = input.char_indices().peekable();
+    while let Some((index, c)) = iter.next() {
+        let start = index;
+        if c.is_digit(RADIX) {
+            let end = from_fn(|| iter.next_if(|(_, c)| c.is_digit(RADIX)))
+                .last()
+                .map_or(start, |(index, _)| index);
+            let number = input[start..=end].parse().unwrap_or_default();
+            tokens.push_back(Token::Number(number));
+        } else {
+            let end = from_fn(|| iter.next_if(|(_, c)| !c.is_digit(RADIX)))
+                .last()
+                .map_or(start, |(index, _)| index);
+            let identifier = &input[start..=end];
+            tokens.push_back(Token::Identifier(identifier));
+        }
+    }
+    tokens
+}
+
+#[derive(Clone, Debug)]
+pub(crate) enum Token<'a> {
+    Identifier(&'a str),
+    Number(u8),
+}
--- tests/test.rs
+++ tests/test.rs
@@ -1,9 +1,10 @@
 use fatty_acid_macro::fatty_acid;
 
-fatty_acid!(16:0);
-fatty_acid!(18:0);
-fatty_acid!(18:1 { 9 => DC });
-fatty_acid!(18:2 { 9 => DC, 12 => DC });
+fatty_acid!(C18U0);
+fatty_acid!(C18U2DC9DC12);
+// fatty_acid!(18:0);
+// fatty_acid!(18:1 { 9 => DC });
+// fatty_acid!(18:2 { 9 => DC, 12 => DC });
 
 #[test]
 fn test() {
