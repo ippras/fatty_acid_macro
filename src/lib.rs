dbce8ee90d269653bb765fedcbbab0d8896d0c12
kgv <kgv@users.noreply.github.com>
2025-07-08 13:00:17 +0300

    0

--- src/lib.rs
+++ src/lib.rs
@@ -1,30 +1,43 @@
-use self::parser::{Token, tokenize};
 use proc_macro::TokenStream;
-use quote::{format_ident, quote};
+use quote::quote;
 use syn::{
-    Attribute, Ident, Visibility,
+    Ident, LitInt, Token, braced,
     parse::{Parse, ParseStream, Result},
-    parse_macro_input, token,
+    parse_macro_input,
+    punctuated::Punctuated,
+    token::Brace,
 };
 
 struct Input {
-    attributes: Vec<Attribute>,
-    visibility: Visibility,
-    identifier: Ident,
-    // _semi_token: token::Semi,
+    carbon: Ident,
+    _brace_token: Brace,
+    entries: Punctuated<Entry, Token![,]>,
 }
 
 impl Parse for Input {
     fn parse(input: ParseStream) -> Result<Self> {
-        let attributes = input.call(Attribute::parse_outer)?;
-        let visibility = input.parse()?;
-        let identifier = input.parse()?;
-        // let _semi_token = input.parse()?;
+        let carbon = input.parse()?;
+        let content;
         Ok(Self {
-            attributes,
-            visibility,
-            identifier,
-            // _semi_token,
+            carbon,
+            _brace_token: braced!(content in input),
+            entries: content.parse_terminated(Entry::parse, Token![,])?,
+        })
+    }
+}
+
+struct Entry {
+    index: LitInt,
+    _arrow_token: Token![=>],
+    key: Ident,
+}
+
+impl Parse for Entry {
+    fn parse(input: ParseStream) -> Result<Self> {
+        Ok(Entry {
+            index: input.parse()?,
+            _arrow_token: input.parse()?,
+            key: input.parse()?,
         })
     }
 }
@@ -32,44 +45,38 @@ impl Parse for Input {
 #[proc_macro]
 pub fn fatty_acid(tokens: TokenStream) -> TokenStream {
     let Input {
-        attributes,
-        visibility,
-        identifier,
-        ..
+        carbon, entries, ..
     } = parse_macro_input!(tokens as Input);
-    let input = identifier.to_string();
-    let mut tokens = tokenize(&input);
-    let Some(Token::Identifier("C")) = tokens.pop_front() else {
-        panic!("the fatty acid must start with the carbons keyword `C`");
-    };
-    let Some(Token::Number(carbon)) = tokens.pop_front() else {
-        panic!("`C` must be followed by the number of carbons");
-    };
-    let carbon = carbon as u8;
-    let Some(Token::Identifier("U")) = tokens.pop_front() else {
-        panic!("the carbons must be followed by the unsaturated keyword `U`");
-    };
-    let Some(Token::Number(unsaturated)) = tokens.pop_front() else {
-        panic!("`U` must be followed by the number of unsaturated bounds");
-    };
-    assert_eq!(
-        unsaturated as usize * 2,
-        tokens.len(),
-        "the unsaturated not equal indices length"
-    );
+    let carbon = carbon.to_string();
+    if !carbon.starts_with('C') {
+        panic!("parse carbon keyword (`C`), {carbon}");
+    }
+    let carbon = carbon
+        .trim_start_matches('C')
+        .parse::<u8>()
+        .expect(&format!("parse carbon number, {carbon}"));
     let mut double_bounds_index = vec![];
     let mut double_bounds_parity = vec![];
     let mut triple_bounds_index = vec![];
-    while let Some(Token::Identifier(identifier)) = tokens.pop_front() {
-        let Some(Token::Number(index)) = tokens.pop_front() else {
-            panic!("the unsaturated bound must have an index");
-        };
+    for Entry {
+        index,
+        _arrow_token,
+        key,
+    } in entries
+    {
+        let index = index
+            .base10_parse::<i8>()
+            .expect(&format!("parse entry index {index}"));
         let index = if index != 0 {
             quote!(Some(#index))
         } else {
             quote!(None)
         };
-        match identifier {
+        match &*key.to_string() {
+            "D" => {
+                double_bounds_index.push(index);
+                double_bounds_parity.push(quote!(None));
+            }
             "DC" => {
                 double_bounds_index.push(index);
                 double_bounds_parity.push(quote!(Some(false)));
@@ -78,17 +85,12 @@ pub fn fatty_acid(tokens: TokenStream) -> TokenStream {
                 double_bounds_index.push(index);
                 double_bounds_parity.push(quote!(Some(true)));
             }
-            "D" => {
-                double_bounds_index.push(index);
-                double_bounds_parity.push(quote!(None));
-            }
             "T" => {
                 triple_bounds_index.push(index);
             }
-            identifier => panic!("unexpected identifier {identifier}"),
+            key => panic!("unexpected entry key {key}"),
         }
     }
-    // let identifier = format_ident!("{identifier}");
     let expanded = quote! {{
         df! {
             "Carbon" => Series::new(PlSmallStr::EMPTY, [#carbon]),
@@ -105,5 +107,3 @@ pub fn fatty_acid(tokens: TokenStream) -> TokenStream {
     }};
     TokenStream::from(expanded)
 }
-
-mod parser;
--- tests/test.rs
+++ tests/test.rs
@@ -8,11 +8,10 @@ use polars::prelude::*;
 
 #[test]
 fn test() {
-    let c18u0 = fatty_acid!(C18U0);
-    println!("C18U0: {c18u0:?}");
-    let c18u3dc9dt12d15 = fatty_acid!(C18U3DC9DT12D15);
-    println!("C18U3DC9DT12D15: {c18u3dc9dt12d15:?}");
-    let c18u4dc0dt0d0t0 = fatty_acid!(C18U4DC0DT0D0T0);
-    println!("C18U4DC0DT0D0T0: {c18u4dc0dt0d0t0:?}");
-    // println!("C18U2DC9DC12: {C18U2DC9DC12:?}");
+    let c18u0 = fatty_acid!(C18 {});
+    println!("c18u0: {c18u0:?}");
+    let c18u3dc9dt12d15 = fatty_acid!(C18 { 9 => DC, 12 => DT, 15 => D });
+    println!("c18u3dc9dt12d15: {c18u3dc9dt12d15:?}");
+    let c18u4dc0dt0d0t0 = fatty_acid!(C18 { 0 => D, 0 => DC, 0 => DT, 0 => T });
+    println!("c18u4dc0dt0d0t0: {c18u4dc0dt0d0t0:?}");
 }
